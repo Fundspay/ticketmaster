cc45ddb325f1c2e1401f468e7a70c0f5240aa335
AryaMehta03 <aryaamitmehta@gmail.com>
2025-02-22 23:06:46 +0530

    feat: bottom nav in home screen to toggle between event list and main page, also changed the initial route for testing and added event registration screen

---
+++ lib/src/features/home/presentation/main_home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'home_screen.dart'; // This is ticket list screen
+import 'package:ticketmaster/src/features/event/presentation/event_list_screen.dart'; // Event screen
+
+class MainHomeScreen extends ConsumerStatefulWidget {
+  const MainHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<MainHomeScreen> createState() => _MainHomeScreenState();
+}
+
+class _MainHomeScreenState extends ConsumerState<MainHomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),      // Ticket list screen
+    EventListScreen(), // Event list screen
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Events',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/src/routing/app_routes.dart
+++ lib/src/routing/app_routes.dart
@@ -1,19 +1,19 @@
-// lib/src/routing/app_routes.dart
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../features/auth/presentation/login_screen.dart';
 import '../features/auth/presentation/register_screen.dart';
+import '../features/home/presentation/main_home_screen.dart'; // Updated import
 import '../features/event/presentation/event_list_screen.dart';
-import '../features/home/presentation/home_screen.dart';
+import '../features/registration/presentation/event_registration_screen.dart';
 import '../features/settings/presentation/settings_screen.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home',
   routes: [
     GoRoute(
       path: '/',
       name: 'login',
-      builder: (context, state) => LoginScreen(),
+      builder: (context, state) => const LoginScreen(),
     ),
     GoRoute(
       path: '/register',
@@ -23,12 +23,17 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/home',
       name: 'home',
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const MainHomeScreen(),
     ),
     GoRoute(
       path: '/events',
       name: 'eventList',
       builder: (context, state) => const EventListScreen(),
+    ),
+    GoRoute(
+      path: '/event-registration',
+      name: 'eventRegistration',
+      builder: (context, state) => const EventRegistrationScreen(),
     ),
     GoRoute(
       path: '/settings',
